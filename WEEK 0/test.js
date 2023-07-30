2d8f77f82e644e6c1a037c2c76682650fde8a394
hamdyproj1 <92056439+hamdyproj1@users.noreply.github.com>
2023-07-30 21:26:17 +0300

    Create test.js

---
+++ WEEK 0/test.js
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Dragons</title>
+	<link rel="stylesheet" type="text/css" href="index.css">
+</head>
+<body>
+	<header>
+		<h1>Dragons</h1>
+	</header>
+	<div class="container">
+		<nav>
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">About</a></li>
+				<li><a href="#">Contact</a></li>
+			</ul>
+		</nav>
+		<div class="content">
+			<section>
+				<h2>Section Heading</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac adipiscing elit. Integer euismod libero eu orci commodo, eu placerat sapien consequat. Sed sit amet tellus mollis, pharetra magna eu, blandit libero. Nullam tristique ipsum eu nunc placerat, vel dapibus nunc accumsan. Nullam quis maximus urna. Quisque quis nisi vitae eros lacinia dictum. Aliquam ut arcu at odio bibendum blandit. Nullam euismod dapibus enim, ac blandit felis euismod ut.</p>
+				<img src="telegram.svg" alt="Image" style="float: right;">
+			</section>
+		</div>
+		<div class="sidebar">
+			<ul>
+				<li><a href="#">Link 1</a></li>
+				<li><a href="#">Link 2</a></li>
+				<li><a href="#">Link 3</a></li>
+			</ul>
+		</div>
+	</div>
+	<footer>
+		<p>Frontend Task Made by :[YOUR_NAME]</p>
+	</footer>
+</body>
+</html>
